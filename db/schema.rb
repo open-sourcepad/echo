f39e1e4eff7db574be2abb114bb56554b8e1b411
Migz Suelto <migz.suelto@gmail.com>
2015-10-23 15:58:00 +0800

    pg init

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151023063700) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text     "description",        default: "", null: false
     t.string   "image_file_name"
@@ -55,7 +58,7 @@ ActiveRecord::Schema.define(version: 20151023063700) do
     t.string   "authentication_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
